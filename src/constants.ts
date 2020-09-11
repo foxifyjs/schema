de1851e2db8ea5a7e1dc7f24a21ccde6fdcb0bdf
Ardalan Amini <ardalanamini22@gmail.com>
2020-09-11 21:52:53 +0430

    WIP: Improve types

--- src/constants.ts
+++ src/constants.ts
@@ -2,49 +2,62 @@ import type { AnyType } from "./types";
 
 /* ------------------------- Any ------------------------- */
 
-export type Validator<T> = (value: T) => T;
+export type Validator<Type> = (value: Type) => Type;
 
-export type DefaultType<T> = () => T | null;
+export type DefaultType<Type> = () => Type | null;
 
 export type DefaultValue<
-  A extends AnyType<any, any>
-> = A["getDefault"] extends () => infer R ? R : never;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  SchemaT extends AnyType<any, any>
+> = SchemaT["getDefault"] extends () => infer ReturnT ? ReturnT : never;
 
 export type WithDefault<
-  A extends AnyType<any, any>,
-  V extends DefaultType<any>
-> = A & {
-  getDefault: V;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  SchemaT extends AnyType<any, any>,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  Default extends DefaultType<any>
+> = SchemaT & {
+  getDefault: Default;
 };
 
-export type WithRequired<A extends AnyType<any, any>, R extends boolean> = A & {
-  isRequired: R;
+export type WithRequired<
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  SchemaT extends AnyType<any, any>,
+  Required extends boolean
+> = SchemaT & {
+  isRequired: Required;
 };
 
-export type SchemaType<T extends AnyType<any, any>> = WithDefault<
-  T,
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type SchemaType<SchemaT extends AnyType<any, any>> = WithDefault<
+  SchemaT,
   () => null
 >;
 
 export type Result<
-  T,
-  I,
-  A extends AnyType<T, I>,
-  V
-> = A["isRequired"] extends true
-  ? ResultRequired<T, I, A, V>
-  : V extends I
-  ? T
-  : V extends null | undefined
-  ? DefaultValue<A> extends I
-    ? T
+  Type,
+  InputT,
+  SchemaT extends AnyType<Type, InputT>,
+  Value
+> = SchemaT["isRequired"] extends true
+  ? ResultRequired<Type, InputT, SchemaT, Value>
+  : Value extends InputT
+  ? Type
+  : Value extends null | undefined
+  ? DefaultValue<SchemaT> extends InputT
+    ? Type
     : null
   : never;
 
-export type ResultRequired<T, I, A extends AnyType<T, I>, V> = V extends I
-  ? T
-  : V extends null | undefined
-  ? DefaultValue<A> extends I
-    ? T
+export type ResultRequired<
+  Type,
+  InputT,
+  SchemaT extends AnyType<Type, InputT>,
+  Value
+> = Value extends InputT
+  ? Type
+  : Value extends null | undefined
+  ? DefaultValue<SchemaT> extends InputT
+    ? Type
     : never
   : never;
